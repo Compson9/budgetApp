83b66215c4a93b1ff32e68425e2b43679ded2af8
Elvis Compson <105922474+Compson9@users.noreply.github.com>
2025-02-01 21:15:22 +0000

    The learning curve is on

--- script.js
+++ script.js
@@ -1,94 +1,104 @@
-// Select DOM elements
-const transactionEl = document.querySelector(".transactions");
-const balanceNumberEl = document.querySelector(".balance-number");
-const numberIncomeEl = document.querySelector(".number--income");
-const numberExpensesEl = document.querySelector(".number--expenses");
-const formEl = document.querySelector(".form");
-const inputDescriptionEl = document.querySelector(".input--description");
-const inputAmountEl = document.querySelector(".input--amount");
+// // Select DOM elements
+// const transactionEl = document.querySelector(".transactions");
+// const balanceNumberEl = document.querySelector(".balance-number");
+// const numberIncomeEl = document.querySelector(".number--income");
+// const numberExpensesEl = document.querySelector(".number--expenses");
+// const formEl = document.querySelector(".form");
+// const inputDescriptionEl = document.querySelector(".input--description");
+// const inputAmountEl = document.querySelector(".input--amount");
 
-function submitHandler(e) {
-    e.preventDefault();
-    const description = inputDescriptionEl.value;
-    const amount = +inputAmountEl.value;
+// function submitHandler(e) {
+//     e.preventDefault();
+//     const description = inputDescriptionEl.value;
+//     const amount = +inputAmountEl.value;
 
-    // create new transaction Html
-    const transactionItemHTML = `
-    <li class="transaction transaction--${amount > 0 ? "income" : "expense"}">
-          <span class="transaction__text">${description}</span>
-          <span class="transaction__amount">${amount > 0 ? "+" : ""}${amount}</span>
-          <button class="transaction__btn">X</button>
-        </li>
-    `;
-    // insert Html
-    transactionEl.insertAdjacentHTML("beforeend", transactionItemHTML);
+//     // create new transaction Html
+//     const transactionItemHTML = `
+//     <li class="transaction transaction--${amount > 0 ? "income" : "expense"}">
+//           <span class="transaction__text">${description}</span>
+//           <span class="transaction__amount">${amount > 0 ? "+" : ""}${amount}</span>
+//           <button class="transaction__btn">X</button>
+//         </li>
+//     `;
+//     // insert Html
+//     transactionEl.insertAdjacentHTML("beforeend", transactionItemHTML);
 
-    // clear input fields
-    inputDescriptionEl.value = "";
-    inputAmountEl.value = "";
+//     // clear input fields
+//     inputDescriptionEl.value = "";
+//     inputAmountEl.value = "";
 
-    // unfocus input
-    inputDescriptionEl.blur();
-    inputAmountEl.blur();
+//     // unfocus input
+//     inputDescriptionEl.blur();
+//     inputAmountEl.blur();
 
-    // update income and expenses
-    if (amount > 0) {
-        const currentIncome = +numberIncomeEl.textContent;
-        const updateIncome = currentIncome + amount;
-        numberIncomeEl.textContent = updateIncome;
-    } else {
-        const currentExpenses = +numberExpensesEl.textContent;
-        const updateExpenses = currentExpenses - amount;
-        numberExpensesEl.textContent = updateExpenses;
-    }
+//     // update income and expenses
+//     if (amount > 0) {
+//         const currentIncome = +numberIncomeEl.textContent;
+//         const updateIncome = currentIncome + amount;
+//         numberIncomeEl.textContent = updateIncome;
+//     } else {
+//         const currentExpenses = +numberExpensesEl.textContent;
+//         const updateExpenses = currentExpenses - amount;
+//         numberExpensesEl.textContent = updateExpenses;
+//     }
 
-    // update Balance
-    const income = +numberIncomeEl.textContent;
-    const expenses = +numberExpensesEl.textContent;
-    const updateBalance = income - expenses;
-    balanceNumberEl.textContent = updateBalance;
+//     // update Balance
+//     const income = +numberIncomeEl.textContent;
+//     const expenses = +numberExpensesEl.textContent;
+//     const updateBalance = income - expenses;
+//     balanceNumberEl.textContent = updateBalance;
+// }
 
-    // check if balance is negative
-    // if (updateBalance <= 0) {
-    //     balanceNumberEl.style.color = "red";
-    // }
-}
+// // submit form
+// formEl.addEventListener("submit", submitHandler);
 
-// submit form
-formEl.addEventListener("submit", submitHandler);
+// transactionEl.addEventListener("click", function removeItem(e) {
+//     // remove transaction
+//     e.preventDefault();
+//     const clickedEl = e.target.parentNode;
+//     if (!clickedEl.classList.contains("transaction")) return;
 
-transactionEl.addEventListener("click", function removeItem(e) {
-    // remove transaction
-    e.preventDefault();
-    const clickedEl = e.target.parentNode;
-    if (!clickedEl.classList.contains("transaction")) return;
+//     const amountEl = clickedEl.querySelector(".transaction__amount");
+//     const amount = +amountEl.textContent;
 
-    const amountEl = clickedEl.querySelector(".transaction__amount");
-    const amount = +amountEl.textContent;
+//     clickedEl.remove();
 
-    clickedEl.remove();
+//     // Update income and expenses
+//     if (amount > 0) {
+//         const currentIncome = +numberIncomeEl.textContent;
+//         const updateIncome = currentIncome - amount;
+//         numberIncomeEl.textContent = updateIncome;
+//     } else {
+//         const currentExpenses = +numberExpensesEl.textContent;
+//         const updateExpenses = currentExpenses + amount;
+//         numberExpensesEl.textContent = updateExpenses;
+//     }
 
-    // Update income and expenses
-    if (amount > 0) {
-        const currentIncome = +numberIncomeEl.textContent;
-        const updateIncome = currentIncome - amount;
-        numberIncomeEl.textContent = updateIncome;
-    } else {
-        const currentExpenses = +numberExpensesEl.textContent;
-        const updateExpenses = currentExpenses + amount;
-        numberExpensesEl.textContent = updateExpenses;
-    }
+//     // update Balance
+//     const income = +numberIncomeEl.textContent;
+//     const expenses = +numberExpensesEl.textContent;
+//     const updateBalance = income - expenses;
+//     balanceNumberEl.textContent = updateBalance;
 
-    // update Balance
-    const income = +numberIncomeEl.textContent;
-    const expenses = +numberExpensesEl.textContent;
-    const updateBalance = income - expenses;
-    balanceNumberEl.textContent = updateBalance;
+//     // check if balance is negative
+//     if (updateBalance <= 0) {
+//         balanceNumberEl.style.color = "red";
+//     }
+// });
 
-    // check if balance is negative
-    if (updateBalance <= 0) {
-        balanceNumberEl.style.color = "red";
-    }
-});
+
+const title = document.querySelector(".first-heading");
+
+const balanceNumber= document.querySelector(".balance-number");
+
+balanceNumber.addEventListener("click",  clickHandler =()=>{
+    
+    balanceNumber.innerHTML = '<h1>Unavailable</h1>'
+})
+
+
+
+
+
 
 
