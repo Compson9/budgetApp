9c98350840e1cef42ad9e812d555609b3041757c
Elvis Compson <105922474+Compson9@users.noreply.github.com>
2025-02-07 16:24:33 +0000

    First commit

--- script.js
+++ script.js
@@ -1,95 +1,4 @@
-// // Select DOM elements
-// const transactionEl = document.querySelector(".transactions");
-// const balanceNumberEl = document.querySelector(".balance-number");
-// const numberIncomeEl = document.querySelector(".number--income");
-// const numberExpensesEl = document.querySelector(".number--expenses");
-// const formEl = document.querySelector(".form");
-// const inputDescriptionEl = document.querySelector(".input--description");
-// const inputAmountEl = document.querySelector(".input--amount");
-
-// function submitHandler(e) {
-//     e.preventDefault();
-//     const description = inputDescriptionEl.value;
-//     const amount = +inputAmountEl.value;
-
-//     // create new transaction Html
-//     const transactionItemHTML = `
-//     <li class="transaction transaction--${amount > 0 ? "income" : "expense"}">
-//           <span class="transaction__text">${description}</span>
-//           <span class="transaction__amount">${amount > 0 ? "+" : ""}${amount}</span>
-//           <button class="transaction__btn">X</button>
-//         </li>
-//     `;
-//     // insert Html
-//     transactionEl.insertAdjacentHTML("beforeend", transactionItemHTML);
-
-//     // clear input fields
-//     inputDescriptionEl.value = "";
-//     inputAmountEl.value = "";
-
-//     // unfocus input
-//     inputDescriptionEl.blur();
-//     inputAmountEl.blur();
-
-//     // update income and expenses
-//     if (amount > 0) {
-//         const currentIncome = +numberIncomeEl.textContent;
-//         const updateIncome = currentIncome + amount;
-//         numberIncomeEl.textContent = updateIncome;
-//     } else {
-//         const currentExpenses = +numberExpensesEl.textContent;
-//         const updateExpenses = currentExpenses - amount;
-//         numberExpensesEl.textContent = updateExpenses;
-//     }
-
-//     // update Balance
-//     const income = +numberIncomeEl.textContent;
-//     const expenses = +numberExpensesEl.textContent;
-//     const updateBalance = income - expenses;
-//     balanceNumberEl.textContent = updateBalance;
-// }
-
-// // submit form
-// formEl.addEventListener("submit", submitHandler);
-
-// transactionEl.addEventListener("click", function removeItem(e) {
-//     // remove transaction
-//     e.preventDefault();
-//     const clickedEl = e.target.parentNode;
-//     if (!clickedEl.classList.contains("transaction")) return;
-
-//     const amountEl = clickedEl.querySelector(".transaction__amount");
-//     const amount = +amountEl.textContent;
-
-//     clickedEl.remove();
-
-//     // Update income and expenses
-//     if (amount > 0) {
-//         const currentIncome = +numberIncomeEl.textContent;
-//         const updateIncome = currentIncome - amount;
-//         numberIncomeEl.textContent = updateIncome;
-//     } else {
-//         const currentExpenses = +numberExpensesEl.textContent;
-//         const updateExpenses = currentExpenses + amount;
-//         numberExpensesEl.textContent = updateExpenses;
-//     }
-
-//     // update Balance
-//     const income = +numberIncomeEl.textContent;
-//     const expenses = +numberExpensesEl.textContent;
-//     const updateBalance = income - expenses;
-//     balanceNumberEl.textContent = updateBalance;
-
-//     // check if balance is negative
-//     if (updateBalance <= 0) {
-//         balanceNumberEl.style.color = "red";
-//     }
-// });
-
-
-
-
-// Selecting the items from the html
+// Select DOM elements
 const transactionEl = document.querySelector(".transactions");
 const balanceNumberEl = document.querySelector(".balance-number");
 const numberIncomeEl = document.querySelector(".number--income");
@@ -98,30 +7,84 @@ const formEl = document.querySelector(".form");
 const inputDescriptionEl = document.querySelector(".input--description");
 const inputAmountEl = document.querySelector(".input--amount");
 
+function submitHandler(e) {
+    e.preventDefault();
+    const description = inputDescriptionEl.value;
+    const amount = +inputAmountEl.value;
 
+    // create new transaction Html
+    const transactionItemHTML = `
+    <li class="transaction transaction--${amount > 0 ? "income" : "expense"}">
+          <span class="transaction__text">${description}</span>
+          <span class="transaction__amount">${amount > 0 ? "+" : ""}${amount}</span>
+          <button class="transaction__btn">X</button>
+        </li>
+    `;
+    // insert Html
+    transactionEl.insertAdjacentHTML("beforeend", transactionItemHTML);
 
-// Remove transaction element
-transactionEl.addEventListener("click", function(event){
-    const clickedEl = event.target.parentNode;
-    clickedEl.remove();
+    // clear input fields
+    inputDescriptionEl.value = "";
+    inputAmountEl.value = "";
 
-    // update the income or expenses
+    // unfocus input
+    inputDescriptionEl.blur();
+    inputAmountEl.blur();
+
+    // update income and expenses
+    if (amount > 0) {
+        const currentIncome = +numberIncomeEl.textContent;
+        const updateIncome = currentIncome + amount;
+        numberIncomeEl.textContent = updateIncome;
+    } else {
+        const currentExpenses = +numberExpensesEl.textContent;
+        const updateExpenses = currentExpenses - amount;
+        numberExpensesEl.textContent = updateExpenses;
+    }
+
+    // update Balance
+    const income = +numberIncomeEl.textContent;
+    const expenses = +numberExpensesEl.textContent;
+    const updateBalance = income - expenses;
+    balanceNumberEl.textContent = updateBalance;
+}
+
+// submit form
+formEl.addEventListener("submit", submitHandler);
+
+transactionEl.addEventListener("click", function removeItem(e) {
+    // remove transaction
+    e.preventDefault();
+    const clickedEl = e.target.parentNode;
+    if (!clickedEl.classList.contains("transaction")) return;
+
     const amountEl = clickedEl.querySelector(".transaction__amount");
     const amount = +amountEl.textContent;
 
+    clickedEl.remove();
 
-const currentIncome = +numberIncomeEl.textContent
-const updatedIncome = currentIncome - amount 
-numberIncomeEl.textContent = updatedIncome
+    // Update income and expenses
+    if (amount > 0) {
+        const currentIncome = +numberIncomeEl.textContent;
+        const updateIncome = currentIncome - amount;
+        numberIncomeEl.textContent = updateIncome;
+    } else {
+        const currentExpenses = +numberExpensesEl.textContent;
+        const updateExpenses = currentExpenses + amount;
+        numberExpensesEl.textContent = updateExpenses;
+    }
 
-})
+    // update Balance
+    const income = +numberIncomeEl.textContent;
+    const expenses = +numberExpensesEl.textContent;
+    const updateBalance = income - expenses;
+    balanceNumberEl.textContent = updateBalance;
 
-
-
-
-
-
-
+    // check if balance is negative
+    if (updateBalance <= 0) {
+        balanceNumberEl.style.color = "red";
+    }
+});
 
 
 
